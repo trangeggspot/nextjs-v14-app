5662a9bda947de80f5788b676d377f8ebc9d4f1b
trangeggspot <trang.nguyen@eggspot.app>
2024-05-08 00:34:53 +0700

    remove license

--- src/count-display.ts
+++ src/count-display.ts
@@ -1,8 +1,4 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
+
 
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
