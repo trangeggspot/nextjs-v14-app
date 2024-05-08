b6b2f8b1ce77a21dad67e59fbc87a3327bbce097
trangeggspot <trang.nguyen@eggspot.app>
2024-05-08 08:37:27 +0700

    web component only

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <WebComponent></WebComponent>
-      <ReactComponent></ReactComponent>
+      {/* <ReactComponent></ReactComponent> */}
     </main>
   );
 }
